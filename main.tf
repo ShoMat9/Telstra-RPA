faa3e4e1cd0ce97ab5141e5cb04cea75f7001990
Shobhit Mathur <shobhitmathur18@gmail.com>
2023-11-09 14:25:32 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.79.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  # Configuration options
+resource "azurerm_resource_Group" "testRG"
+{
+name = "TestRG"
+location = "Australia Southeast"
 }
