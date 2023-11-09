971dcc74a02ad9aea129ea3ab9a76d539b0916cc
Shobhit Mathur <shobhitmathur18@gmail.com>
2023-11-09 15:00:16 +1100

    aa

---
+++ OLD/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "testRG" {
+name = "TestRG"
+location = "Australia Southeast"
+}
--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
-resource "azurerm_resource_group" "testRG" {
-name = "TestRG"
-location = "Australia Southeast"
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "myResourceGrouptest1" # Replace with your desired resource group name
+  location = "East US"         # Replace with your desired Azure region
+}
+
+# Output the ID of the resource group
+output "resource_group_id" {
+  value = azurerm_resource_group.example.id
 }
