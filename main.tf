f9874d47b663136dd16469825f4ce3f0a8d91823
Shobhit Mathur <shobhitmathur18@gmail.com>
2023-11-09 15:19:31 +1100

    aa

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
+<#
 resource "azurerm_resource_group" "testRG" {
 name = "TestRG"
+location = "Australia Southeast"
+}
+/#>
+resource "azurerm_resource_group" "notest" {
+name = "notest"
 location = "Australia Southeast"
 }
